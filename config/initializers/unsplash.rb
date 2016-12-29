7cd7a7060cb6d03c204cf497b0ee3a3a86b37aee
umer <umerswati100@gmail.com>
2016-12-29 15:10:22 +0500

    initial commit

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,5 @@
+Unsplash.configure do |config|
+  config.application_id     = "4eaf649031f54a7146bf69f7c6e5ec72ef35b60d5d2571650d3b5f53a98f812c"
+  config.application_secret = "085408f73b2166839b3097f0def68233d623bb79126d2f6f4e2fcaeda801bab1"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+end
